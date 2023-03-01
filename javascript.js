34fa1d8ba63f3b2a9e853c20327c66d273ef8b5e
nlpz404 <marcosnicolaslopez@gmail.com>
2023-02-28 22:31:27 -0300

    Correcciones menores al contenido y estilo. Se implementó javascript.

---
+++ javascript.js
@@ -0,0 +1,36 @@
+document.getElementById("expandir1").addEventListener('click', function() {
+     document.getElementById("celda1").style.height = "auto";
+     document.getElementById("expandir1").style.transform = "rotate(180deg)";
+     document.getElementById("expandir1").style.borderRadius = "50% 50% 0 0";
+})
+
+document.getElementById("expandir2").addEventListener('click', function() {
+    document.getElementById("celda2").style.height = "auto";
+    document.getElementById("expandir2").style.transform = "rotate(180deg)";
+    document.getElementById("expandir2").style.borderRadius = "50% 50% 0 0";
+})
+
+document.getElementById("expandir3").addEventListener('click', function() {
+    document.getElementById("celda3").style.height = "auto";
+    document.getElementById("expandir3").style.transform = "rotate(180deg)";
+    document.getElementById("expandir3").style.borderRadius = "50% 50% 0 0";
+})
+
+document.getElementById("expandir4").addEventListener('click', function() {
+    document.getElementById("celda4").style.height = "auto";
+    document.getElementById("expandir4").style.transform = "rotate(180deg)";
+    document.getElementById("expandir4").style.borderRadius = "50% 50% 0 0";
+})
+
+// document.getElementById("expandir1").addEventListener('click', function() {
+//     if (document.getElementById("expandir1").style.borderRadius = "0 0 50% 50%") {
+//         document.getElementById("celda1").style.height = "auto";
+//         document.getElementById("expandir1").style.transform = "rotate(180deg)";
+//         document.getElementById("expandir1").style.borderRadius = "50% 50% 0 0";
+// }
+//     else if (document.getElementById("expandir1").style.borderRadius = "50% 50% 0 0") {
+//         document.getElementById("celda1").style.height = "400px";
+//         document.getElementById("expandir1").style.transform = "rotate(180deg)";
+//         document.getElementById("expandir1").style.borderRadius = "0 0 50% 50%";
+// }
+// })
